5c0c6a8347c3c5f4b2a293fa417a707c174f9242
Chloe A. Rodriguez <carodriguez@WS-ACADBUS.EDU>
2020-01-29 08:47:05 -0800

    129

---
+++ 128LabArt1/boid.js
@@ -0,0 +1,12 @@
+class Boid{x
+  constructor (x,y,w){
+    this.segments = []
+    this.loc=createVector(x,y);
+    this.vel = createVector(0,0);
+    this.w = snakeWidth;
+    //this.size=size
+    this.clr = color(random(255), random(255), random(255));
+
+}
+run(){}
+ Boid{}
