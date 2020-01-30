e73d69cd85147ac4ee5f9f12adc592567a782848
Chloe A. Rodriguez <carodriguez@WS-ACADBUS.EDU>
2020-01-30 08:39:19 -0800

    art1

--- 128LabArt1/sketch.js
+++ 128LabArt1/sketch.js
@@ -6,12 +6,21 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
-  fill(200, 30, 150);
+  loadBoids(12);
 
 
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
+  background(5,5,5)
+  for(var i = 0; i<boids.length; i++){
+    boids[i].run();
+  }
+}
 
+function loadBoids(n) {
+  for(var i=0; i<n; i++){
+    boids[i]= new Boid(random(width))
+  }
 }
