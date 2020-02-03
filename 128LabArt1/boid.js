c448c08acf2be8b399b8608ed7e2d28ee4bed493
Chloerod-stang <54385645+Chloerod-stang@users.noreply.github.com>
2020-02-03 01:11:23 -0800

    Update boid.js

--- 128LabArt1/boid.js
+++ 128LabArt1/boid.js
@@ -1,11 +1,24 @@
 class Boid{
   constructor (x,y){
     this.loc=createVector(x,y);
-    this.vel = createVector(0,0);
-    //this.size=size
-    this.clr = color(random(255), random(255), random(255));
+    this.vel = createVector(dx,dy);
+    this.acc = createVector(0,0);
+    this.clr = color(random(0, 256), random(0, 256), random(0, 256));
 
 }
+  
 run(){
+  this.update();
+  this.checkEdges();
+  this.render();
+}
+ update() {
+  this.vel.add(this.acc)
+   this.loc.add(this.vel)
+}
 
+  checkEdges() {
+    if(this.loc.x<0){
+      this.vel.x=-1*this.vel.x;
+    
 }
