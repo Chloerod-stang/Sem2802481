4838410e63acb9f93e2d0e7717133b6f26394f5c
Chloerod-stang <54385645+Chloerod-stang@users.noreply.github.com>
2020-02-03 19:06:55 -0800

    Update boid.js

--- 128LabArt1/boid.js
+++ 128LabArt1/boid.js
@@ -20,5 +20,16 @@ run(){
   checkEdges() {
     if(this.loc.x<0){
       this.vel.x=-1*this.vel.x;
-    
-}
+} //left side of screen
+    if(this.loc.x>width){
+      this.vel.x = -1*this.vel.x;
+} //right side of screen
+    if(this.loc.y = -1*this.vel.y;
+} //top of screen
+    if(this.loc.y>height){
+      this.vel.y=-1*this.vel.y;
+    }
+  }
+  
+  render() {
+    for(var i = 0;
