7168f3a7bf206d8a0612a920a60acdffde34b061
Chloe A. Rodriguez <carodriguez@WS-ACADBUS.EDU>
2020-02-04 13:12:54 -0800

    kjahf

--- 128LabArt1/boid.js
+++ 128LabArt1/boid.js
@@ -1,11 +1,38 @@
 class Boid{
   constructor (x,y){
     this.loc=createVector(x,y);
-    this.vel = createVector(0,0);
-    //this.size=size
-    this.clr = color(random(255), random(255), random(255));
-
+    this.vel = createVector(dx,dy);
+    this.acc = createVector(0,0);
+    this.clr = color(random(0, 256), random(0, 256), random(0, 256));
 }
-run(){
 
+run(){
+  this.update();
+  this.checkEdges();
+  this.render();
+}
+
+update(){
+  this.vel.add(this.acc)
+  this.locc.add(this.vel)
+}
+
+checkEdges(){
+  if(this.loc.x<0){
+    this.vel.x=-1*this.vel.x;
+    //left side of screen
+  }
+  if(this.loc.x>width){
+    this.vel.x = -1*this.vel.x;
+  }//right side of screen
+
+  if(this.loc.y<0){
+    this.vel.y=-1*this.vel.y
+  }
+}
+
+render(){
+  for(var i=0; i<boids.length; i++){
+    let d = dist(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y)
+  }
 }
