56935a11bb6568887208e2cd4a2c02592ddbc953
Chloerod-stang <54385645+Chloerod-stang@users.noreply.github.com>
2020-02-10 21:30:20 -0800

    Update sketch.js

--- 128LabArt1/sketch.js
+++ 128LabArt1/sketch.js
@@ -1,27 +1,10 @@
-//  Your Name
-// 	Date or version number
+//  Chloe Rodriguez Stangle
+// 	1/31/20
 //  This is a comment
 //  The setup function function is called once when your program begins
 
 
-boid = []
-
-function setup() {
-  var cnv = createCanvas(800, 800);
-  cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
-  loadBoids(12);
-
-
-}
-
-//  The draw function is called @ 30 fps
-function draw() {
-  background(5,5,5)
-  for(var i = 0; i<boids.length; i++){
-    boids[i].run();
-  }
-}boids = []
+boids = []
 
 // Create a canvas and place it in the center of the screen
 // Call function "loadBoids()" to load an array of boids (create a method for this purpose)
@@ -38,18 +21,9 @@ function loadBoids(){
 }
 
 // Run all the boids in the global Array of Boids
-function draw() {
-  background(220);
-  if (this.id > -1){//not sure
-  for (i = 0; i < boids.length; i++) {//making it run
-    boids[i].run()
-  }  
-}
-
-function loadBoids(n) {
-  for(var i=0; i<n; i++){
-
-    boid[i]= new Boid(random(width), random(height), random(-4, 4), random(-4, 4))
-  }
-
+function draw(){
+  // background(220);
+    for (i = 0; i < boids.length; i++) {//making it run
+      boids[i].run()
+    }  
 }
