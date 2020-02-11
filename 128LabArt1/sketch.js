b452e2e9fc07bbb6f2ac71de3d8aa0aaf638d162
Chloerod-stang <54385645+Chloerod-stang@users.noreply.github.com>
2020-02-10 20:15:02 -0800

    Update sketch.js

--- 128LabArt1/sketch.js
+++ 128LabArt1/sketch.js
@@ -2,11 +2,10 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
-//<<<<<<< HEAD
 
-//=======
+
 boid = []
-//>>>>>>> 44bac0aa6c6b6acc40e39634f766f2415c36edbe
+
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
@@ -22,14 +21,35 @@ function draw() {
   for(var i = 0; i<boids.length; i++){
     boids[i].run();
   }
+}boids = []
+
+// Create a canvas and place it in the center of the screen
+// Call function "loadBoids()" to load an array of boids (create a method for this purpose)
+function setup() {
+  createCanvas(400, 400);
+  loadBoids();
+}
+
+function loadBoids(){
+  for (let i = 0; i < 10; i++) {
+    boid = new Boid(random(0, 200), random(0, 200), random(0, 10), (random(0, 10)))
+    boids.push(boid);
+  }
+}
+
+// Run all the boids in the global Array of Boids
+function draw() {
+  background(220);
+  if (this.id > -1){//not sure
+  for (i = 0; i < boids.length; i++) {//making it run
+    boids[i].run()
+  }  
 }
 
 function loadBoids(n) {
   for(var i=0; i<n; i++){
-//<<<<<<< HEAD
-//=======
+
     boid[i]= new Boid(random(width), random(height), random(-4, 4), random(-4, 4))
-//>>>>>>> 44bac0aa6c6b6acc40e39634f766f2415c36edbe
   }
 
 }
